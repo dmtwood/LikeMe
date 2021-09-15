b8e5e53b58b31d7c439a4fe5709620c17d59e629
dmtwood <dimitri.gevers@gmail.com>
2021-09-15 10:19:29 +0200

    second

--- functions.py
+++ functions.py
@@ -6,7 +6,11 @@ say_hello('dimi')
 
 
 
-def say_hi(naam):
+def say_hi(naam, age=None):
+    if age is not None:
+        return naam, age
     return naam
 
 print(say_hi('jessy'))
+print(say_hi('jessy', 45))
+# print(list(say_hi('abc', 123)) to return a list (of just return [naam, age] al line 11
