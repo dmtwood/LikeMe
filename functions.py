296eb092d9b523428a8b52cde1fac4fdabc4a0b0
dmtwood <dimitri.gevers@gmail.com>
2021-09-15 10:02:14 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+def say_hello(name):
+    print(name)
+
+
+say_hello('dimi')
+
+
+
+def say_hi(naam):
+    return naam
+
+print(say_hi('jessy'))
---
+++ type.py
@@ -0,0 +1,15 @@
+x=1
+y=2
+a=4
+b=10
+
+a, b = b, a
+
+"""
+help=x
+x=y
+y=help
+
+print(x, y)
+"""
+print(a,b)
