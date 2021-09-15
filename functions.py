46504ecefc1c8d39000a38c3e0c4ace67cdb5394
dmtwood <dimitri.gevers@gmail.com>
2021-09-15 10:29:23 +0200

    second

--- functions.py
+++ functions.py
@@ -14,3 +14,17 @@ def say_hi(naam, age=None):
 print(say_hi('jessy'))
 print(say_hi('jessy', 45))
 # print(list(say_hi('abc', 123)) to return a list (of just return [naam, age] al line 11
+
+
+def say_hi2(naam, age2=None):
+    if age2 is not None:
+        return [naam, age2]
+    return naam
+
+mylist = say_hi2('abc', 123)
+print(mylist[0])
+
+
+def cutsttring(mystring):
+    return mystring[0:3]
+print(cutsttring('mijntest'))
